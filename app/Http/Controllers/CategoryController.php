48f092951a08f73b3e529447f3509f9846cf17a9
shibayaman <shibayaman7374@gmail.com>
2020-06-02 20:36:40 +0900

    Add Category

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Category;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        return Category::all();
+    }
+
+    public function show($id)
+    {
+        return Category::find($id);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::resources([
-    "sake" => "SakeController"
+    "sake" => "SakeController",
+    "category" => "CategoryController"
 ]);
